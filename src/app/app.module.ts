22e6b660a5df5bd86109902c25fc40a2f9e52d7d
natalie_g <natalie.gurvich@gmail.com>
2022-05-28 20:58:16 +0300

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,19 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers, reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { CustomerModule } from './customer/customer.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CustomerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
