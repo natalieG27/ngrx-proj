cd800df7fd8eb963027fc155e57dfa54e3e23e64
natalie_g <natalie.gurvich@gmail.com>
2022-05-28 19:54:36 +0300

    add customer actions

---
+++ src/app/customer/store/action/customer.actions.ts
@@ -0,0 +1,21 @@
+import { createAction, props } from '@ngrx/store';
+import { Customer } from '../../../models/customer';
+
+// export const loadCustomers = createAction(
+//   '[Customer] Load Customers'
+// );
+
+// export const loadCustomersSuccess = createAction(
+//   '[Customer] Load Customers Success',
+//   props<{ data: any }>()
+// );
+
+// export const loadCustomersFailure = createAction(
+//   '[Customer] Load Customers Failure',
+//   props<{ error: any }>()
+// );
+
+export const addCustomer = createAction(
+  '[Customer] Add Customer', 
+  (customer: Customer) => ({customer})
+);
